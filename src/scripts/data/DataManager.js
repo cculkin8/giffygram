23a1f7db19078bd1093401aed0915c3813a21026
Carter Culkin <carter.culkin@gmail.com>
2021-03-23 13:55:46 -0500

    Added the delete button and functionality, started on the edit button functionality

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -1,13 +1,36 @@
 export const getUsers = () => {
-
-    return fetch("http://localhost:8088/users")
-    .then(response => response.json())
+	return fetch("http://localhost:8088/users")
+	.then(response => response.json())
 }
 
+let postCollection = [];
+
+export const usePostCollection = () => {
+  //Best practice: we don't want to alter the original state, so
+  //make a copy of it and then return it
+  //The spread operator makes this quick work
+  return [...postCollection];
+}
 export const getPosts = () => {
-    return fetch("http://localhost:8088/posts")
+  return fetch("http://localhost:8088/posts")
     .then(response => response.json())
+    .then(parsedResponse => {
+      postCollection = parsedResponse
+      return parsedResponse;
+    })
 }
+
+export const createPost = postObj => {
+	return fetch("http://localhost:8088/posts", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(postObj)
+  
+	})
+		.then(response => response.json())
+  }
 
 const loggedInUser = {
 	id: 1,
@@ -16,5 +39,20 @@ const loggedInUser = {
 }
 
 export const getLoggedInUser = () => {
-	return loggedInUser;
+	return {...loggedInUser};
 }
+export const deletePost = postId => {
+	return fetch(`http://localhost:8088/posts/${postId}`, {
+		method: "DELETE",
+		headers: {
+			"Content-Type": "application/json"
+		}
+  
+	})
+		.then(response => response.json())
+		.then(getPosts)
+  }
+  export const getSinglePost = (postId) => {
+	return fetch(`http://localhost:8088/posts/${postId}`)
+	  .then(response => response.json())
+  }
